3d4f32e0992173e3fd223d767f0369d5f7b0f149
Giovanni De Toni <giovanni.det@gmail.com>
2019-09-03 16:58:52 +0200

    Improve HPC scripts.

--- single_job.sh
+++ single_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#PBS -l select=3:ncpus=12:mem=10GB
-#PBS -l walltime=72:0:0
+#PBS -l select=3:ncpus=10:mem=3GB
+#PBS -l walltime=24:0:0
 #PBS -q common_cpuQ
 #PBS -M giovanni.detoni@studenti.unitn.it
 #PBS -m be
 
-./priority_sort_task/priority_sort_task.py
+./priority_sort_task/priority_sort_task.py -lr=$1 -checkpoint_dir=$2 -sequence_max_length=8
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -4,5 +4,8 @@
 LRATE=("1.000e-04", "7.525e-05", "5.050e-05", "2.575e-05", "1.000e-06")
 
 # Generate a job for each of the learning rates.
+index=0
 for r in ${LRATE[@]}:
-  qsub -- single_job.sh $r
+  TEMP_NAME="./learning_rate_$index"
+  qsub -- single_job.sh ${r} ${TEMP_NAME}
+  index=$((index+1))
