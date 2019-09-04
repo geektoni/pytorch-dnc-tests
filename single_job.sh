dd21fd1c2093677468048f62569d04e8a72230a5
Giovanni De Toni <giovanni.det@gmail.com>
2019-09-04 10:09:18 +0200

    Final fixes to HPC job files.

--- single_job.sh
+++ single_job.sh
@@ -4,6 +4,8 @@
 #PBS -l walltime=24:0:0
 #PBS -q common_cpuQ
 #PBS -M giovanni.detoni@studenti.unitn.it
+#PBS -V
 #PBS -m be
 
+echo "Starting job with learning rate ${learning_rate} and checkpoint directory ${check_dir}"
 ~/master/priority_sort_task/priority_sort_task.py -lr=$learning_rate -checkpoint_dir=$check_dir -sequence_max_length=8
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -5,7 +5,10 @@ LRATE=("1.000e-04", "7.525e-05", "5.050e-05", "2.575e-05", "1.000e-06")
 
 # Generate a job for each of the learning rates.
 index=0
-for r in ${LRATE[@]}:
-  TEMP_NAME="./learning_rate_$index"
-  qsub -q common_cpuQ -v learning_rate=${r},check_dir=${TEMP_NAME} singe_job.sh
+for r in ${LRATE[@]}; do
+  TEMP_NAME="./learning_rate_${index}"
+  export learning_rate="${r}"
+  export check_dir="${TEMP_NAME}"
+  qsub -V -N "DNC_priority_${index}" -q common_cpuQ single_job.sh
   index=$((index+1))
+done
