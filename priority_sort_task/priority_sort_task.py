f8a56262eda200f82dfecdde55319b3e53fdfff7
Giovanni De Toni <giovanni.det@gmail.com>
2019-10-02 16:47:22 +0200

    When random_length_sequence is used, the length is sampled from 2 to sequence_max_length/2+1

--- priority_sort_task/priority_sort_task.py
+++ priority_sort_task/priority_sort_task.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
         optimizer.zero_grad()
 
         if args.random_length_sequence:
-            random_length = np.random.randint(2, sequence_max_length//2)
+            random_length = np.random.randint(2, sequence_max_length//2+1)
         else:
             random_length = sequence_max_length
 
